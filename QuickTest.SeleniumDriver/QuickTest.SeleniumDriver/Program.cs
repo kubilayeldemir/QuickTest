cebf476cbe8d6a95df56e44e57f54ee38c165d63
kubilayeldemir <kubi0707@gmail.com>
2021-06-19 01:03:34 +0300

    feat: C# Selenium driver initial commit

---
+++ QuickTest.SeleniumDriver/QuickTest.SeleniumDriver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuickTest.SeleniumDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
