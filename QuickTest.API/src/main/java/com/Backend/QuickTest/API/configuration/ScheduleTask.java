e9139000908475ec415ec9b18f29fa215f635c9d
kubilayeldemir <kubi0707@gmail.com>
2021-06-23 20:29:26 +0300

    feat: Enable task scheduling for spring

---
+++ QuickTest.API/src/main/java/com/Backend/QuickTest/API/configuration/ScheduleTask.java
@@ -0,0 +1,9 @@
+package com.Backend.QuickTest.API.configuration;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+@Configuration
+@EnableScheduling
+public class ScheduleTask {
+}
