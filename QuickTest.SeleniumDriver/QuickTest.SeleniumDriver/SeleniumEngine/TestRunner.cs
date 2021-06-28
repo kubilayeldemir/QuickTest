c3a32898eca6db28cd658ec2d1e0484278317114
kubilayeldemir <kubi0707@gmail.com>
2021-06-28 18:03:06 +0300

    feat: TestRunner try test step and catch errors scenario impl.

--- QuickTest.SeleniumDriver/QuickTest.SeleniumDriver/SeleniumEngine/TestRunner.cs
+++ QuickTest.SeleniumDriver/QuickTest.SeleniumDriver/SeleniumEngine/TestRunner.cs
@@ -6,6 +6,7 @@ using DataAccess.Repositories;
 using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -23,68 +24,85 @@ namespace QuickTest.SeleniumDriver.SeleniumEngine
             
             var driver = new DriverFactory().createWebDriver();
 
+            bool isError = false;
             var testReport = new TestReport();
             testReport.testSteps = new List<ReportStep>();
             testReport.testcaseId = testCase.id;
-            testReport.status = false;
+            testReport.status = true;
             testReport.testStartDate = DateTime.Now;
-
-            
-            foreach(Step step in testCase.steps)
+            var sortedSteps = testCase.steps.OrderBy(x => x.stepNumber);
+            foreach (Step step in sortedSteps)
             {
                 var testStep = new ReportStep();
                 testStep.stepNumber = step.stepNumber;
-                testStep.status = false;
+                testStep.status = true;
                 testStep.stepDate = DateTime.Now;
                 testStep.stepDescription = step.ToString();
-                switch(step.action)
-                {
-                    case TestAction.GoTo:
-                        driver.Navigate().GoToUrl(step.actionText);
-                        break;
-                    case TestAction.Click:
-                        By by;
-                        if (step.by == FindElementBy.ClassName)
-                        {
-                            by = By.ClassName(step.elementAddress);
-                        }
-                        else if(step.by == FindElementBy.Xpath)
-                        {
-                            by = By.XPath(step.elementAddress);
-                        }
-                        else
-                        {
-                            by = By.Id(step.elementAddress);
-                        }
-                        driver.FindElement(by).Click();
-                        break;
-                    case TestAction.SendText:
-                        if (step.by == FindElementBy.ClassName)
-                        {
-                            by = By.ClassName(step.elementAddress);
-                        }
-                        else if (step.by == FindElementBy.Xpath)
-                        {
-                            by = By.XPath(step.elementAddress);
-                        }
-                        else
-                        {
-                            by = By.Id(step.elementAddress);
-                        }
-                        driver.FindElement(by).SendKeys(step.actionText);
-                        break;
+                try
+                {                    
+                    switch (step.action)
+                    {
+                        case TestAction.GoTo:
+                            driver.Navigate().GoToUrl(step.actionText);
+                            break;
+                        case TestAction.Click:
+                            By by;
+                            if (step.by == FindElementBy.ClassName)
+                            {
+                                by = By.ClassName(step.elementAddress);
+                            }
+                            else if (step.by == FindElementBy.Xpath)
+                            {
+                                by = By.XPath(step.elementAddress);
+                            }
+                            else
+                            {
+                                by = By.Id(step.elementAddress);
+                            }
+                            driver.FindElement(by).Click();
+                            break;
+                        case TestAction.SendText:
+                            if (step.by == FindElementBy.ClassName)
+                            {
+                                by = By.ClassName(step.elementAddress);
+                            }
+                            else if (step.by == FindElementBy.Xpath)
+                            {
+                                by = By.XPath(step.elementAddress);
+                            }
+                            else
+                            {
+                                by = By.Id(step.elementAddress);
+                            }
+                            driver.FindElement(by).SendKeys(step.actionText);
+                            break;
+                    }
+                    testStep.status = true;
                 }
-                testStep.status = true;
-                Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
-                string fileName = testCase.testName + DateTime.Now.ToString()+".PNG";
-                string imageLink = await AzureStorageHelper.UploadFileToStorage(fileName, ss.AsByteArray);
-                testStep.stepImageUrl = imageLink;
-                testReport.testSteps.Add(testStep);
-                Thread.Sleep(1000);
+                catch (Exception e)
+                {
+                    testStep.status = false;
+                    testReport.status = false;
+                    isError = true;
+                    testReport.errorMessage = e.Message.ToString();
+                }
+                finally
+                {
+                    Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
+                    string fileName = testCase.testName + DateTime.Now.ToString() + ".PNG";
+                    fileName = fileName.Replace(' ', '_');
+                    string imageLink = await AzureStorageHelper.UploadFileToStorage(fileName, ss.AsByteArray);
+                    testStep.stepImageUrl = imageLink;
+                    testReport.testSteps.Add(testStep);
+                    Thread.Sleep(1000);
+                }
+                if (isError)
+                {
+                    break;
+                }
             }
             Thread.Sleep(5000);
             testReport.testEndDate = DateTime.Now;
-            testReport.status = true;
             driver.Quit();
             return testReport;
         }
