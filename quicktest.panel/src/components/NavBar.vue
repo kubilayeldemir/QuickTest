37c6c65e32f9d1cd4fa4edc4b73fb4e63e45c662
kubilayeldemir <kubi0707@gmail.com>
2021-07-02 21:20:27 +0300

    refactor: Panel create navbar component

---
+++ quicktest.panel/src/components/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="container-fluid">
+      <router-link :to="{name:'Homepage'}" class="nounderline">
+            <a class="navbar-brand">QuickTest</a>
+      </router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+}
+</script>
+
+<style scoped>
+.nounderline {
+  text-decoration: none !important
+}
+</style>
